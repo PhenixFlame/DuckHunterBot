35bece4a51478a1909ba185f0bbefcab33414e7e
an <diver841@gmail.com>
2020-11-10 20:13:18 +0300

    CREATE: clients:     DiscrodClient(token)     Post(TextChannel, delay=0.5)

---
+++ clients.py
@@ -0,0 +1,59 @@
+import discord
+from abc_ import Publisher
+from logger import getLogger
+from datetime import datetime, timedelta
+import asyncio
+
+# ____________CONSTANTS________________
+
+POST_MESSAGE_PERIOD = 0.5  # seconds
+now = datetime.now
+
+# ____________END_CONSTANTS____________
+
+
+class DiscordClient(discord.Client, Publisher):
+    logger = getLogger('DiscordClient')
+
+    async def on_ready(self):
+        self.logger.debug('Logged on as {0}!'.format(self.user))
+
+    async def on_message_edit(self, before, after):
+        await self._publish(after)
+
+    async def on_message(self, message):
+        await self._publish(message)
+
+
+class Post:
+    """
+    Interface for control sending messages processes
+    """
+    messageInterval: timedelta
+    nextTimeSend = None
+
+    def __init__(self, channel: discord.TextChannel, delay=POST_MESSAGE_PERIOD):
+        """
+        :param channel: discord.TextChannel will used for send messages
+        :param delay: in seconds
+        """
+        self.messageInterval = timedelta(seconds=delay)
+        self.channel = channel
+        self.nextTimeSend = now()
+
+    async def send(self, channel: discord.TextChannel, text):
+        """
+        Send text to channel, controlling time interval between messages
+        :param channel:
+        :param text:
+        :return:
+        """
+
+        # check message last time
+        wait_time = (self.nextTimeSend - now()).total_seconds()
+        if wait_time > 0:
+            await asyncio.sleep(wait_time)
+
+        message = await channel.send(text)
+        self.nextTimeSend = message.created_at + self.messageInterval
+
