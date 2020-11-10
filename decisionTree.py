b8a97498d5c488d30685344b4830bc6c6874e7be
an <diver841@gmail.com>
2020-11-10 23:12:57 +0300

    MOVE: DecisionTree class to events.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,31 +1,4 @@
-from logger import AsyncLogger
-import discord
-
-
-class NoDecisionError(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
-class DecisionTree:
-    def __init__(self, tree_dict):
-        self.logger = AsyncLogger(type(self).__name__)
-        self.tree_dict = tree_dict
-
-    async def decision(self, message):
-        return await self._decision(message, self.tree_dict)
-
-    async def _decision(self, message: discord.Message, tree):
-        try:
-            for check, next_tree in tree.items():
-                if await check(message):
-                    return await self._decision(message, next_tree)
-        except Exception as e:
-            if tree is None:
-                await self.logger.info(f'{e} for message: \n\t{message.content}')
-
-            return tree
-
+DECISION_TREE_DICT = {}
 
 '''
 BabyDuckEvent
--- events.py
+++ events.py
@@ -1,17 +1,36 @@
 from abc_ import Publisher, Subscriber, Visitor
-from decisionTree import DecisionTree
-from discord import TextChannel, Message
 from logger import AsyncLogger
+import discord
+
+
+class DecisionTree:
+    def __init__(self, tree_dict):
+        self.logger = AsyncLogger(type(self).__name__)
+        self.tree_dict = tree_dict
+
+    async def decision(self, message):
+        return await self._decision(message, self.tree_dict)
+
+    async def _decision(self, message: discord.Message, tree):
+        try:
+            for check, next_tree in tree.items():
+                if await check(message):
+                    return await self._decision(message, next_tree)
+        except Exception as e:
+            if tree is None:
+                await self.logger.info(f'{e} for message: \n\t{message.content}')
+
+            return tree
 
 
 class EventManager(Publisher, Subscriber):
     decisionTree = tuple()
 
-    def __init__(self, channel: TextChannel, decisionTree: DecisionTree):
+    def __init__(self, channel: discord.TextChannel, decisionTree: DecisionTree):
         self.channel = channel
         self.decisionTree = decisionTree
 
-    async def receive(self, message: Message):
+    async def receive(self, message: discord.Message):
         if self.channel == message.channel:
             event = await self.decisionTree.decision(message)
             if event:
@@ -22,6 +41,13 @@ class Event(Visitor):
     def __init__(self, name):
         self.name = name
         self.logger = AsyncLogger(type(self).__name__)
+
+
+class NoDecisionError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
 
 
 
