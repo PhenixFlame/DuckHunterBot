77f5f877222a540f7d712fbe16dafe56dfea320f
an <diver841@gmail.com>
2020-11-11 20:31:56 +0300

    CREATE: decisionTree.py: author_checker, TEST_AUTHOR_DECISION_TREE_DICT

--- decisionTree.py
+++ decisionTree.py
@@ -1,4 +1,20 @@
-DECISION_TREE_DICT = {}
+import discord
+from config import SELF_NAME, DUCK_HUNT_BOT_NAME
+
+
+def author_checker(author):
+    async def cheker(message: discord.Message):
+        return str(message.author) == author
+    return cheker
+
+
+TEST_AUTHOR_DECISION_TREE_DICT = {
+    author_checker(SELF_NAME): "MyMessageEvent",
+    author_checker(DUCK_HUNT_BOT_NAME): "DuckHuntEvent"
+}
+
+DECISION_TREE_DICT = {
+}
 
 '''
 BabyDuckEvent
