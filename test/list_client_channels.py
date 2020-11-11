a56bf416961b05847ad0291e75f851c2b972226c
an <diver841@gmail.com>
2020-11-11 17:10:07 +0300

    CREATE: TEST: list_channels

---
+++ test/list_client_channels.py
@@ -0,0 +1,29 @@
+from clients import DiscordClient
+import discord
+import funcsource as fs
+from config import settings
+import asyncio
+
+
+TOKEN = settings['token']['bot']
+
+client = DiscordClient()
+loop = client.loop
+
+
+async def get_hystory():
+    print('wait client ready')
+    await client.ready.wait()
+    print('client is ready')
+    channels = client.channels
+    # channels = {c.name: c for c in channels}
+    print(channels)
+    # print(fs.write_file('resources/channels.yaml', {c.name: c.id for c in channels}, 'yaml'))
+    print('file saved')
+
+loop.create_task(get_hystory())
+client.run(TOKEN)
+
+
+
+
