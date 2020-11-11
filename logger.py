b415ace93f8dd885a47941205abd34aaf0b3df07
an <diver841@gmail.com>
2020-11-11 16:24:57 +0300

    CREATE: logger.loggerDictConfig

--- logger.py
+++ logger.py
@@ -1,5 +1,32 @@
 import logging
 import asyncio
+from logging.config import dictConfig
+
+loggerDictConfig = {
+  "formatters": {
+    "detailed": {
+      "class": "logging.Formatter",
+      "datefmt": "%m/%d/%Y %I:%M:%S",
+      "format": "[%(asctime)s] %(relativeCreated)dms : %(name)s : %(levelname)s : %(message)s"
+    }
+  },
+  "handlers": {
+    "console": {
+      "class": "logging.StreamHandler",
+      "formatter": "detailed",
+      "level": "DEBUG"
+    }
+  },
+  "root": {
+    "handlers": [
+      "console"
+    ],
+    "level": "DEBUG"
+  },
+  "version": 1
+}
+
+dictConfig(loggerDictConfig)
 
 
 class AsyncLogger:
@@ -13,9 +40,7 @@ class AsyncLogger:
 
     async def run(self, msg, type_):
         loop = asyncio.get_running_loop()
-        asyncio.create_task(
-            loop.run_in_executor(self.Executor, getattr(self.logger, type_), msg)
-        )
+        loop.run_in_executor(self.Executor, getattr(self.logger, type_), msg)
 
     async def debug(self, msg):
         await self.run(msg, 'debug')
