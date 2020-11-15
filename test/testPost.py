294e9fe22c1b7787f6abfb981f8311b3ae617daf
an <diver841@gmail.com>
2020-11-15 22:48:40 +0300

    TESTS: HOT TEST ADD: clients.Post: testPost.py

---
+++ test/testPost.py
@@ -0,0 +1,22 @@
+from config import TOKEN, BOTFLAG, GUILD_HUNT, CHANNEL_HUNT
+from events import EventManager, DecisionTree
+from hunter import DuckHunter
+from clients import DiscordClient, Post
+from logger import AsyncLogger
+client = DiscordClient()
+
+
+async def initialization():
+    await client.ready.wait()
+
+    HuntChannel = client.channels[GUILD_HUNT][CHANNEL_HUNT]
+    logger = AsyncLogger('initialization')
+    post = Post(HuntChannel, delay=5)
+    for i in ['hello'] + [str(i) for i in range(10)]:
+        logger.debug(f'send "{i}"')
+        await post.send(i)
+
+    await client.close()
+
+client.loop.create_task(initialization())
+client.run(TOKEN, bot=BOTFLAG)
