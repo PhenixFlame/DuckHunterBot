2072225d5641337cb3f421fd8fee4b57f9870c0c
an <diver841@gmail.com>
2020-11-16 16:35:58 +0300

    MAIN: first test launch

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from config import TOKEN, BOTFLAG, GUILD_HUNT, CHANNEL_HUNT
+from config import TOKEN, BOTFLAG, GUILD_HUNT, CHANNEL_HUNT, DECISION_TREE
 from events import EventManager, DecisionTree
 from hunter import DuckHunter
 from clients import DiscordClient, Post
-from decisionTree import DECISION_TREE_DICT
+from logger import AsyncLogger
+from funcsource import log_errors
 
 client = DiscordClient()
 
@@ -10,16 +11,22 @@ client = DiscordClient()
 async def initialization():
     await client.ready.wait()
 
-    HuntChannel = client.channels[GUILD_HUNT][CHANNEL_HUNT]
+    logger = AsyncLogger('initialization')
+    with log_errors(logger):
+        logger.debug('start initialization')
+        HuntChannel = client.channels[GUILD_HUNT][CHANNEL_HUNT]
 
-    post = Post(HuntChannel)
+        post = Post(HuntChannel)
 
-    decision_tree = DecisionTree(DECISION_TREE_DICT)
-    eventmanager = EventManager(HuntChannel, decision_tree)
-    duck_hunter = DuckHunter(post)
+        decision_tree = DecisionTree(DECISION_TREE)
+        eventmanager = EventManager(HuntChannel, decision_tree)
+        duck_hunter = DuckHunter(post)
 
-    eventmanager.subscrive(duck_hunter)
-    client.subscrive(eventmanager)
+        eventmanager.subscrive(duck_hunter)
+        client.subscrive(eventmanager)
+
+        client.loop.create_task(duck_hunter.main_loop())
+        logger.debug('end initialization')
 
 client.loop.create_task(initialization())
 client.run(TOKEN, bot=BOTFLAG)
